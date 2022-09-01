d4bdf337be1db1b66f3dd3b60a976a3390115f51
cw417 <chriswhitlock417@gmail.com>
2022-09-01 17:11:28 -0400

    Add keypress functionality

--- src/components/GroceryInput.js
+++ src/components/GroceryInput.js
@@ -14,14 +14,25 @@ export default function GroceryInput({ addGrocery }) {
     groceryAmount.current.value = null
     groceryName.current.value = null
   }  
+  
+  function handleKeyPress(event) {
+    /**
+     * Calls 'handleAddRecipe' when 'Enter' key is pressed.
+     * The keycode for 'Enter' is 13.
+     * @param event  Keypress event watching for 'Enter' key.
+     */
+    if (event.keyCode === 13 || event.which === 13) {
+      handleAddGrocery()
+    }
+  }
 
   return (
     <div className='flex justify-center m-4'>
       <div className='flex justify-end mr-4 w-1/4'> 
-        <input className='inpt amt' ref={groceryAmount} type='text' placeholder='Amt' /> 
+        <input className='inpt amt' ref={groceryAmount} onKeyPress={handleKeyPress} type='text' placeholder='Amt' /> 
       </div>
       <div className=''>
-         <input className='inpt' ref={groceryName} type='text' placeholder='Name' />
+         <input className='inpt' ref={groceryName} onKeyPress={handleKeyPress} type='text' placeholder='Name' />
         </div>
       <div>
          <button className='btn' onClick={handleAddGrocery}><FiPlus /></button>
